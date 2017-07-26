cfb1b9943e3a2aef125ab07a7e9a0df6c8890578
Aminul <aminul2050@gmail.com>
2017-07-26 15:44:51 +0600

    Added Login page inside login directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import {LoginComponent} from './login.component';
+import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home.component';
 
 @NgModule({
